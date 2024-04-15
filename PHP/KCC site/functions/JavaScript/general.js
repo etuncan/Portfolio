2ff22f920de2066b120dd767806e0feeaa455cda
etuncan <etuncan094@gmail.com>
2024-04-15 18:55:06 -0500

    Add files via upload

--- PHP/KCC site/functions/JavaScript/general.js
+++ PHP/KCC site/functions/JavaScript/general.js
@@ -5,8 +5,35 @@ document.addEventListener("contextmenu",
 		}
 	}
 );
+function showMenu(){
+	document.getElementById('menu-2-content').classList.toggle('hide-menu');
+}
+//Contrast Accessibility function
+function dayNight(){
+	let x=getComputedStyle(document.documentElement).getPropertyValue('--bg-color-op-1');
+	let y=getComputedStyle(document.documentElement).getPropertyValue('--bg-color');
+	if(x===y){
+		document.documentElement.style.setProperty('--bg-color','var(--bg-color-op-2)');
+		document.documentElement.style.setProperty('--text-color','var(--text-color-op-2)');
+		document.documentElement.style.setProperty('--border-color','var(--border-color-op-2)');
+	}
+	else{
+		document.documentElement.style.setProperty('--bg-color','var(--bg-color-op-1)');
+		document.documentElement.style.setProperty('--text-color','var(--text-color-op-1)');
+		document.documentElement.style.setProperty('--border-color','var(--border-color-op-1)');
+	}
+}
+//Large Text Accessibility function
+function largeText(){
+	document.getElementById('access-article').classList.toggle('text-large');
+}
 
-let dvh=window.innerHeight*0.01;
-document.documentElement.style.setProperty("--dvh",`${dvh}px`);
-window.addEventListener("resize",()=>{let dvh=window.innerHeight*0.01;
-document.documentElement.style.setProperty("--dvh",`${dvh}px`);});
+function dyslexiaFont(){
+	document.getElementById('access-article').classList.toggle('dyslexia-font');
+}
+
+
+//let dvh=window.innerHeight*0.01;
+//document.documentElement.style.setProperty("--dvh",`${dvh}px`);
+//window.addEventListener("resize",()=>{let dvh=window.innerHeight*0.01;
+//document.documentElement.style.setProperty("--dvh",`${dvh}px`);});
