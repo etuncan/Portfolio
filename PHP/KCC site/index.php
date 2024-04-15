75920c5f6e11fad0c85cbdf783ac24dd400f7bea
etuncan <etuncan094@gmail.com>
2024-04-15 18:54:03 -0500

    Add files via upload

--- PHP/KCC site/index.php
+++ PHP/KCC site/index.php
@@ -1,43 +1,40 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>Kit & Crow Creations</title>
+		<title>Kit+Crow</title>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="preconnect" href="https://fonts.gstatic.com">
-		<link rel="preconnect" href="https://fonts.googleapis.com">	
-		<link rel="stylesheet" type="text/css" href="./theme/general.css">
-		<link rel="stylesheet" type="text/css" href="./theme/content.css">
-		<link rel="stylesheet" type="text/css" href="./theme/fonts.css">		
 		<link rel="stylesheet" type="text/css" href="./theme/theme.css">
-		<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Coda&display=swap">
-		<link rel="icon" href="./images/favicon.png">
+		<link rel="stylesheet" type="text/css" href="./theme/heading.css">	
+
 	</head>
 	<body>
 		<div id="container">
 			<?php include('./theme/header.html');?>
 			<main class="wrapper-1">
-				<div id="mainlogo">
-					<img class="no-click" src="./images/kcc_main_logo_large.v1.png"/>
-				</div>
-				<div id="menulogo">
-					<a href="./pages/.html">
-						<img src="./images/sl_letter_left_1w.png"/>
-						<img src="./images/sl_letter_left_2o.png"/>
-					</a>
-					<img src="./images/sl_split_1w.png"/>
-					<a href="./pages.html">
-						<img src="./images/sl_letter_mid_1w.png"/>
-						<img src="./images/sl_letter_mid_2o.png"/>
-					</a>
-					<img src="./images/sl_split_1w.png"/>
-					<a href="./Blog/blog.html">
-						<img src="./images/sl_letter_right_1w.png"/>
-						<img src="./images/sl_letter_right_2o.png"/>
-					</a>			
+				<img class="banner" src="./images/bgpt3_1.png" alt="banner"/>
+				<div class="wrapper-2">
+					<h1>We Focus On</h1>
+					<section class="breakln home-sect">
+					<img class="list-img" src="./images/Asset 15.svg"/>
+					<h2>Adaptable Design</h2>
+					<p>Technology is always evolving, and few more quickly than computers and the internet. This is why we carefully balance complexity, modularity, and replacability in all of our work. To ensure that when your website needs change, it is as simple as possible to update.</p>
+					</section>
+					
+					<section class="breakln home-sect">
+					<img class="list-img" src="./images/Asset 5.svg"/>
+					<h2>Professional Look</h2>
+					<p>Your website is the face of your business. Before a potential client or customer experiences your service or see the quality of your products they will likely see your website. With the inundation of online shopping and social media, that likelihood becomes closer to a guarantee. You don’t need redesign your brand or add a million plug-ins. We can help adjust your existing site, keeping it elegantly simplistic, while still letting your unique brand shine through. Best of all, it is all tailored to your needs. No bloating with unnecessary features, just what you need to help your business flourish. </p>
+					</section>
+					<br>
+					<section class="home-sect">
+					<img class="list-img" src="./images/access.png"/>
+					<h2>Accessability in Mind</h2>
+					<p>We want everyone to be able to experience the sites we build, that means we build with accessability in mind. We strickly follow W3O guidelines in order to provide access to everyone possible.</p>
+					</section>
 				</div>
 			</main>
-			<?php include('./theme/footer.php');?>
+			<?php include('./theme/footer.html');?>
 		</div><!--#container-->
 		<script type="text/javascript" src="./functions/JavaScript/general.js"></script>
 	</body>
--- PHP/KCC site/template.php
+++ PHP/KCC site/template.php
@@ -1,32 +1,32 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>Kit & Crow Creations/About</title>
+		<title>Kit+Crow</title>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="preload" as="style" type="text/css" href="./theme/fonts.css">
 		<link rel="preconnect" href="https://fonts.gstatic.com">
 		<link rel="preconnect" href="https://fonts.googleapis.com">	
-		<link rel="stylesheet" type="text/css" href="./theme/general.css">
 		<link rel="stylesheet" type="text/css" href="./theme/heading.css">
-		<link rel="stylesheet" type="text/css" href="./theme/content.css">
+		<link rel="stylesheet" type="text/css" href="./theme/theme.css">
 		<link rel="stylesheet" type="text/css" href="./theme/fonts.css">
 		<link rel="stylesheet" type="text/css" href="./theme/about.css">
+		<?php $query=htmlentities($_SERVER['QUERY_STRING']);
+				parse_str(html_entity_decode($query), $arr);
+				$arr=filter_var_array($arr,FILTER_SANITIZE_ENCODED);
+				extract($arr);
+		?>
+		<link rel="stylesheet" type="text/css" href="./pages/<?php echo $page;?>.css">
 		<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&family=Open+Sans:wght@400;500&family=Coda&display=swap">
 		<link rel="icon" href="./images/favicon.png">
 	</head>
 	<body>
-		<?php include("./theme/header.html");?>
 		<div id="container">
-			<?php $query=htmlentities($_SERVER['QUERY_STRING']);
-				parse_str(html_entity_decode($query), $arr);
-				$arr=filter_var_array($arr,FILTER_SANITIZE_ENCODED);
-				extract($arr);
-				include("./pages/$page.html");
-			?>
+			<?php include("./theme/header.html");?>
+			<?php include("./pages/$page.html");?>
 			<?php include("./theme/footer.html"); ?>
 			
 		</div>
-		<script type="text/javascript" src="../files_gen/general_functions_001.js"></script>
+		<script type="text/javascript" src="./functions/JavaScript/general.js"></script>
 	</body>
 </html>
