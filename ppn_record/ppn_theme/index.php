8618b727a331847c1da3e458a8a3511f1fe46be8
etuncan <etuncan094@gmail.com>
2026-04-13 12:03:46 -0500

    Update index.php

--- ppn_record/ppn_theme/index.php
+++ ppn_record/ppn_theme/index.php
@@ -1,29 +1,30 @@
-get_header(); ?>
+<?php get_header(); ?>
 
 <div id="main-content" class="main-content">
 
-<?php echo ppn_page_title(); //TODO: create function?>
+<?php echo ppn_page_title(); /*LOC:functions.php*/ ?>
 
 	<div class="block-content">
 		<div class="container">
-			<div class="<?php echo esc_attr(implode(' ', $panel_classes)); ?>">
-				<div class="<?php echo esc_attr($center_classes); ?>">
+			<div class="<?php /*echo esc_attr(implode(' ', $panel_classes));*/ /*TODO:update panel classes*/ ?>">
+				<div class="<?php /*echo esc_attr($center_classes);*/ /*TODO:update center classes*/ ?>">
 				<?php
-					if ( have_posts() ) {
-
-						if(!is_singular()) { wp_enqueue_style('ppn-blog'); echo '<div class="blog blog-style-default">'; }
-
-						while ( have_posts() ) : the_post();
-
-							get_template_part( 'content', 'blog-item' );
-
+					if(have_posts()):
+						if(!is_singular()):
+							wp_enqueue_style('ppn-blog'); echo '<div class="blog blog-style-default">';
+						endif;
+						
+						while(have_posts()): 
+							the_post();
+							get_template_part('content','blog-item');
 						endwhile;
-
-						if(!is_singular()) { scalia_pagination(); echo '</div>'; }
-
-					} else {
-						get_template_part( 'content', 'none' );
-					}
+						
+						if(!is_singular()): 
+							/*ppn_pagination(); TODO:change pagination to function*/ echo '</div>';
+						endif;
+					else:
+						get_template_part('content','none');
+					endif;
 				?>
 				</div>
 			</div>
@@ -31,5 +32,4 @@ get_header(); ?>
 	</div><!-- .block-content -->
 </div><!-- #main-content -->
 
-<?php
-get_footer();
+<?php get_footer();?>
