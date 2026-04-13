44a65a97ae4ec35ee2c5d4352dee20c1fa1fa9ac
etuncan <etuncan094@gmail.com>
2026-04-13 13:06:55 -0500

    Update content-blog.php

--- ppn_record/ppn_theme/content-blog.php
+++ ppn_record/ppn_theme/content-blog.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template used for displaying page content in blog
+ * Template part for blog page content
  */
 
 	$item_data = array(
@@ -12,19 +12,19 @@
 		'blog_post_types' => '',
 		'blog_pagination' => ''
 	);
-	$item_data = scalia_get_post_data($item_data, 'page', get_the_ID());
-	$sidebar_position = scalia_check_array_value(array('', 'left', 'right'), $item_data['sidebar_position'], '');
-	$sidebar_stiky = $item_data['sidebar_sticky'] ? 1 : 0;
+	$item_data = ppn_get_post_data($item_data, 'page', get_the_ID());
+	//$sidebar_position = ppn_check_array_value(array('', 'left', 'right'), $item_data['sidebar_position'], '');
+	//$sidebar_stiky = $item_data['sidebar_sticky'] ? 1 : 0;
 	$panel_classes = array('panel', 'row');
 	$center_classes = 'panel-center';
-	$sidebar_classes = '';
+	//$sidebar_classes = '';
 ?>
 
 <div class="block-content">
 	<div class="container">
 		<div class="<?php echo esc_attr(implode(' ', $panel_classes)); ?>">
 			<div class="<?php echo esc_attr($center_classes); ?>">
-				<?php scalia_blog(array('blog_style' => $item_data['blog_style'], 'blog_post_per_page' => $item_data['blog_post_per_page'], 'blog_categories' => $item_data['blog_categories'], 'blog_post_types' => $item_data['blog_post_types'], 'blog_pagination' => $item_data['blog_pagination'])); ?>
+				<?php ppn_blog(array('blog_style' => $item_data['blog_style'], 'blog_post_per_page' => $item_data['blog_post_per_page'], 'blog_categories' => $item_data['blog_categories'], 'blog_post_types' => $item_data['blog_post_types'], 'blog_pagination' => $item_data['blog_pagination'])); ?>
 
 				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
@@ -32,7 +32,7 @@
 						<?php
 							the_content();
 							wp_link_pages( array(
-								'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'scalia' ) . '</span>',
+								'before'      => '<div class="page-links"><span class="page-links-title">' . __('Pages:','ppn') . '</span>',
 								'after'       => '</div>',
 								'link_before' => '<span>',
 								'link_after'  => '</span>',
