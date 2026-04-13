ebba76b7e679d43907be37342aa558dc72f3131d
etuncan <etuncan094@gmail.com>
2026-04-13 12:57:56 -0500

    Create page.php

---
+++ ppn_record/ppn_theme/page.php
@@ -0,0 +1,20 @@
+<?php 
+/*
+ * Template for displaying home page
+ */
+?>
+<?php get_header();?>
+
+<div id="main-content" class="main-content">
+
+<?php echo ppn_page_title(); ?>
+
+<?php
+	while (have_posts()) : the_post();
+		get_template_part('content','page');
+	endwhile;
+?>
+
+</div><!-- #main-content -->
+
+<?php get_footer();
