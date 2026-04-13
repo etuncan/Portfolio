6d8e645b7d7195b2d9615f7241c77197e1ae8337
etuncan <etuncan094@gmail.com>
2026-04-13 13:21:06 -0500

    Create single.php

---
+++ ppn_record/ppn_theme/single.php
@@ -0,0 +1,19 @@
+<?php get_header(); ?>
+
+<div id="main-content" class="main-content">
+
+<?php echo ppn_page_title();
+	while(have_posts()):
+		the_post();
+		if(get_post_type() == 'post' || get_post_type() == 'scalia_news'){
+			get_template_part('content','page');
+		} else {
+			get_template_part( 'content', get_post_format() );
+		}
+	endwhile;
+?>
+
+</div><!-- #main-content -->
+
+<?php
+get_footer();
