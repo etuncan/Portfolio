bb5f25112409d5460b6fbce3d121570829d42ae1
etuncan <etuncan094@gmail.com>
2026-05-11 11:13:00 -0500

    Update index.php
    Signed-off-by: etuncan <etuncan094@gmail.com>

--- ppn_record/ppn_theme/index.php
+++ ppn_record/ppn_theme/index.php
@@ -2,12 +2,12 @@
 
 <div id="main-content" class="main-content">
 
-<?php echo ppn_page_title(); /*LOC:functions.php*/ ?>
+<?php echo ppn_page_title(); //Loc:functions.php?>
 
 	<div class="block-content">
 		<div class="container">
-			<div class="<?php /*echo esc_attr(implode(' ', $panel_classes));*/ /*TODO:update panel classes*/ ?>">
-				<div class="<?php /*echo esc_attr($center_classes);*/ /*TODO:update center classes*/ ?>">
+			<div class="<?php //echo esc_attr(implode(' ', $panel_classes)); ?>">
+				<div class="<?php //echo esc_attr($center_classes); ?>">
 				<?php
 					if(have_posts()):
 						if(!is_singular()):
@@ -20,9 +20,9 @@
 						endwhile;
 						
 						if(!is_singular()): 
-							/*ppn_pagination(); TODO:change pagination to function*/ echo '</div>';
+							/*ppn_pagination();*/ echo '</div>';
 						endif;
-					//TODO: add else option
+					//TODO:Add else option
 					endif;
 				?>
 				</div>
@@ -31,4 +31,5 @@
 	</div><!-- .block-content -->
 </div><!-- #main-content -->
 
-<?php get_footer();?>
+<?php
+get_footer();
